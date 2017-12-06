670d56c6ab5e2ad9a6eb903f6346f1e8d6b15218
liuhongyan <beijing_lan@126.com>
2017-12-06 20:04:41 +0800

    init

---
+++ src/main/java/cn/lhy/example/sprintbootlearn/SprintbootLearnApplication.java
@@ -0,0 +1,14 @@
+package cn.lhy.example.sprintbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintbootLearnApplication.class, args);
+		System.out.println("hello world1");
+	}
+
+}
---
+++ src/test/java/cn/lhy/example/sprintbootlearn/SprintbootLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.lhy.example.sprintbootlearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprintbootLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
